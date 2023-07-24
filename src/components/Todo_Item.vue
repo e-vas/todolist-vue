7f442a4039f90bc8bf60a5804196f24764454333
e-vas <96337026+e-vas@users.noreply.github.com>
2023-07-24 12:44:37 +0300

    Add files via upload

---
+++ src/components/Todo_Item.vue
@@ -0,0 +1,14 @@
+<template>
+<li>
+  item 1
+</li>
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Todo_List.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+        {{ todos }}
+        <Todo_Item />
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import Todo_Item from '@/components/Todo_Item.vue'
+export default {
+  props: ['todos'],
+  components: {
+    Todo_Item
+  }
+}
+
+</script>
