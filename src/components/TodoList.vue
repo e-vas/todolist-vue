a73c57007e0a3e7b84d66e6be8caba8a9f52a28b
e-vas <96337026+e-vas@users.noreply.github.com>
2023-08-06 12:37:03 +0300

    Add files via upload

---
+++ src/components/TodoList.vue
@@ -0,0 +1,27 @@
+<template>
+  <li v-for="item in todos" :key="item.id">
+    <input type="checkbox" 
+    
+    v-on:click="$emit('checBoxTask', item.id)"
+    v-bind:checked="item.complited"
+    /> 
+    <span>{{ item.title }}</span>
+    <button
+    
+    v-on:click="$emit('deleteTask', item.id)"
+    > &#x2716; </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: 
+  ['todos'], 
+  
+  components: {
+    
+
+  }
+}
+
+</script>
